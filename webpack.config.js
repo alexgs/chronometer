6815a18536496def07b83c101a13ecedb9226b53
Alex Gates-Shannon <alex.nebula99@gmail.com>
2020-03-14 17:00:40 -0400

    Improve Webpack and Typescript configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devServer: {
@@ -15,12 +16,24 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test:/\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
     ],
   },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+  ],
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ],
   },
