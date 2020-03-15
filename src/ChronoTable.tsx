901401e009e8bedfda647fbfb3f2e37bf394e075
Alex Gates-Shannon <alex.nebula99@gmail.com>
2020-03-15 19:09:26 -0400

    Add ChronoTable stub

---
+++ src/ChronoTable.tsx
@@ -0,0 +1,195 @@
+import * as React from 'react';
+
+export const ChronoTable: React.FunctionComponent = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td />
+          <td>12 PM</td>
+          <td>15</td>
+          <td>30</td>
+          <td>45</td>
+          <td>1 PM</td>
+          <td>15</td>
+          <td>30</td>
+          <td>45</td>
+          <td>2 PM</td>
+          <td>15</td>
+          <td>30</td>
+          <td>45</td>
+          <td>3 PM</td>
+          <td>15</td>
+          <td>30</td>
+          <td>45</td>
+          <td>4 PM</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Working</td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+        </tr>
+        <tr>
+          <td>Gym</td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+        </tr>
+        <tr>
+          <td>Sleeping</td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+          <td>
+            <input type={'radio'} />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+ChronoTable.displayName = 'ChronoTable';
